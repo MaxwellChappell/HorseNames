418e78b8449ef18a7e3856f575b1e1db1da026ba
MaxwellChappell <mjchapp3ll@gmail.com>
2021-02-16 10:30:12 -0600

    finally uploading

--- generation_counter.py
+++ generation_counter.py
@@ -4,6 +4,8 @@ import pprint
 import re
 from dataclasses import dataclass
 from typing import List
+import pandas as pd
+
 http = httplib2.Http()
 
 @dataclass
@@ -21,10 +23,17 @@ class HorseBit:
 count = 0
 
 def horse_set_creator(name, names, gen_start):
+    """
+    Adds to names dictionary every horse in the horse specified ancestry
+    
+    horse: a name of a horse as the end of of a pedigree query link ex. "/grand+fappy"
+    names: a dictionary of all the names processed
+    gen_start: the current generation of the horse
+
+    """
     global count
     count += 1
     expand_names = dict()
-    new_names = dict()
 
     status, response = http.request('https://www.pedigreequery.com/' + name)
     
@@ -41,30 +50,99 @@ def horse_set_creator(name, names, gen_start):
             small = link.find('a',{'class':'horseName'})
             slash = small['href']
             if slash not in names:
-                empty = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+                empty = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
                 names[slash] = HorseBit(small.string, empty)
             names[slash].total_appeared += 1
             names[slash].generations[gen_start+generation] += 1
-            
-    return expand_names
+    return  expand_names
 
-
-
-horse_set = {'/grand+fappy' : HorseBit("GRAND FAPPY", [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],1)}
-new_set = {'/grand+fappy': 1}
-
-while len(new_set) != 0:
-    print("Total: " + str(len(horse_set)))
-    print("To Do: " + str(len(new_set)))
-    print("Done: " + str(count))
-    print()
-    horse = list(new_set.keys())[0]
-    gen = new_set[horse]
-    new_set.pop(horse)
-    new_set |= horse_set_creator(horse, horse_set, gen)
+def save_simple(horse, horse_list):
+    """
+    Creates a simple txt with horse information
     
-file = open("names3.txt", "w+")
-for horse in horse_set:
-    file.write(horse)
-    file.write(horse_set[horse]+'\n')
-file.close()
+    horse: expecting the same pedigree link format, becomes name of .txt
+    horse_list: the whole horse dictionary
+    """
+    file = open(str(horse[1:]) +".txt", "w+")
+    for horse in horse_list:
+        file.write(horse)
+        file.write('\n')
+        file.write(str(horse_list[horse]))
+        file.write('\n')
+    file.close()
+
+def save_names_only(horse, horse_list):
+    """
+    Creates a simple txt with horse names
+    
+    horse: expecting the same pedigree link format, becomes name of .txt
+    horse_list: the whole horse dictionary
+    """
+    file = open(str(horse[1:]) +"_names.txt", "w+")
+    for horse in horse_list:
+        file.write(horse)
+        file.write('\n')
+        file.write(str(horse_list[horse].name))
+        file.write('\n')
+    file.close()
+
+def save_complex_data(horse, horse_list):
+    """
+    Creates a csv and txt with differnt generation data
+    
+    horse: expecting the same pedigree link format, becomes name of .txt
+    horse_list: the whole horse dictionary
+    """
+    data = []
+    for item in horse_list:
+        indiv = [item, horse_list[item].name, horse_list[item].total_appeared]
+        for gen in horse_list[item].generations:
+            indiv.append(gen)
+        data.append(indiv)
+
+    df = pd.DataFrame(data)
+    df.columns = ["Link","Name","Apeared","1st Generation", "2nd Generation", "2rd Generation","4th Generation","5","6","7","8","9","10","11",'12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','43','44','45']
+    df.to_csv(str(horse[1:])+".csv")
+
+    gen_data= {}
+    for i in range(46):
+            gen_data[i] = []
+
+    for item in data:
+            for i in range(3,len(item)-4):
+                    if item[i] > 0:
+                            gen_count = item[i]
+                            gen_percent = item[i] / (2**(i-3))
+                            gen_data[i-3].append([item[0],gen_count, gen_percent])
+
+    file = open(str(horse[1:])+"_generations.txt", "w+")
+    for i in gen_data:
+            file.write(str(i) +":\n")
+            for horse in gen_data[i]:
+                    file.write(horse[0] + ': ' + str(horse[1])+'\n')
+                    file.write(str(horse[2])+'\n')
+                    file.write('\n')
+    file.close()
+
+
+def start(horse_link, horse_name, horse_list):
+    horse_list[horse_link] = HorseBit(horse_name, [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],1)
+    new_names = {horse_link: 0}
+    while len(new_names) != 0:
+        print("Total: " + str(len(horse_set)))
+        print("To Do: " + str(len(new_names)))
+        print("Done: " + str(count))
+        print()
+        horse = list(new_names.keys())[0]
+        gen = new_names[horse]
+        new_names.pop(horse)
+        new_names |= horse_set_creator(horse, horse_set, gen)
+
+link = '/grand+fappy'
+name = 'GRAND FAPPY'
+horse_set = {}
+start(link, name, horse_set)
+save_simple(link, horse_set)
+save_names_only(link, horse_set)
+save_complex_data(link, horse_set)
